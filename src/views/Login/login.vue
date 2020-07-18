266f1a90d737b5d8e3da89dd35cd3c5bd9fec8ce
txj981025@163.com <1828797223@qq.com>
2020-07-18 16:42:00 +0800

    初始化项目，全局样式

---
+++ src/views/Login/login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <h2>登陆页面</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped></style>
